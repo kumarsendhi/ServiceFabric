370bf7199357d9e1e564e7af5e07bcdb0ae32b56
K <kumar.k@hp.com>
2019-02-20 14:12:01 +0530

    Create Service

---
+++ ECommerce.ProductCatalog/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.ProductCatalog.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.ProductCatalog
+{
+    interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task AddProduct(Product product);
+    }
+}
